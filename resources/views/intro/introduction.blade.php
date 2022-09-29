b669b36253650b22959869d887f4a06aa99d6d12
Maeta0616 <maeryo616@gmail.com>
2022-09-29 23:26:51 +0900

    レイアウト編集&introduction

--- resources/views/intro/introduction.blade.php
+++ resources/views/intro/introduction.blade.php
@@ -2,21 +2,43 @@
     <x-slot name="header">
         　    <h1>Introduction</h1>
     </x-slot>
-     <h1 class="text-3xl font-bold">このアプリの使い方</h1>
+     <h1 class="text-4xl font-bold">このアプリの使い方</h1>
      
      <div class="intoroduction">
-         <p class="text-2xl">
+        <p class="text-3xl">
              ①初めに
-         </p>
-         <p class="text-1xl">
-             ユーザー情報を登録してみよう！
-             <br>
-             以下のアンカーから名前、性別、年齢、お気に入り芸人を入力してみましょう！
-             <a href="" class="text-blue-400 hover:text-blue-600 transition-all duration-300">
-                 ユーザーを編集する
-             </a>
-             
-         </p>
+        </p>
+        <p class="text-2xl">
+            ・ユーザー情報を登録してみよう！
+            <br>
+            以下のアンカーから名前、性別、年齢、お気に入り芸人を入力してみましょう！
+            <a href="" class="text-blue-400 hover:text-blue-600 transition-all duration-300">
+                ユーザーを編集する
+            </a>
+        </p>
+        <br>
+        <p class="text-2xl">
+            ・レビューを投稿してみよう！
+            <br>
+            「<a href="/reviews/create" class="text-blue-400 hover:text-blue-600 transition-all duration-300">レビュー作成ページ</a>」
+            からレビューを投稿してみよう！
+            <br>
+            もし、投稿したいネタがプルダウンにない場合は？
+            <br>
+            「<a href"/netas/ncreate" class="text-blue-400 hover:text-blue-600 transition-all duration-300">新規ネタ登録ページ</a>」
+            からネタを登録してから投稿できるようになります！
+            <br>
+            新規ネタ登録ページにて芸人の候補内に登録したい芸人がいない場合は？
+            <br>
+            「<a href="/combinations/ccreate" class="text-blue-400 hover:text-blue-600 transition-all duration-300">新規芸人(ピン・コンビ・トリオ)</a>」
+            から芸人を登録してから投稿できるようになります！
+        </p>
+        <br>
+        <p class="text-2xl">
+            ・ランキングを見てみよう！
+            「<a href="/neta/rank" class="text-blue-400 hover:text-blue-600 transition-all duration-300">ネタランキング</a>」
+            からレビューの評価によるランキングが見られます。ジャンル別でも見られるので是非見てみてください！
+        </p>
      </div>
 </x-app-layout>
     
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,39 +40,39 @@
             <div class="md:flex justify-between mx-auto container items-center box-border h-50 w-50 p-2 border-4 max-w-full">
                     <h1 class="text-3xl text-red-500">Function</h1>
                      
-                     <div class="review page">
-                        <a href="/" class="text-blue-400 hover:text-blue-800 transition-all duration-300">レビューページ</a>
-                         
-                     </div>
+                <div class="review page">
+                    <a href="/" class="text-blue-400 hover:text-blue-800 transition-all duration-300">レビューページ</a>
+                </div>
                      
-                     <div class='review_create'>
+                <div class='review_create'>
                     <a href='/reviews/create' class="text-blue-400 hover:text-blue-800 transition-all duration-300">レビュー作成ページ</a>
-                    </div>
+                </div>
                      
-                     <div class="neta">
+                <div class="neta">
                      <a href='/neta' class="text-blue-400 hover:text-blue-800 transition-all duration-300">ネタ検索ページ</a>
+                </div>
                     
-                    </div>
-                    
-                   <div class='combination_full_page'>
+                <div class='combination_full_page'>
                      <a href='/combination' class="text-blue-400 hover:text-blue-800 transition-all duration-300">お笑いコンビ（トリオ）検索ページ</a>
-                   
-                  </div>
-        
-                  <div class='offices_full_page'>
-                <a href='/office' class="text-blue-400 hover:text-blue-800 transition-all duration-300">芸能事務所一覧ページ</a>
-                   
                 </div>
         
-               <div class='combination_create_page'>
-                  <a href='/combinations/ccreate' class="text-blue-400 hover:text-blue-800 transition-all duration-300">新規お笑いコンビ(トリオ)登録ページ</a>
-                  
-              </div>
+                <!--<div class='offices_full_page'>-->
+                <!--<a href='/office' class="text-blue-400 hover:text-blue-800 transition-all duration-300">芸能事務所一覧ページ</a>-->
+                <!--</div>-->
+                
+                <div class='rank'>
+                    <a href='/neta/rank' class="text-blue-400 hover:text-blue-800 transition-all duration-300">ネタランキング</a>
+                </div>
+                
+                <div class='neta_create_page'>
+                    <a href='/netas/ncreate' class="text-blue-400 hover:text-blue-800 transition-all duration-300">新規ネタ登録ページ</a>
+                </div>
         
-                 <div class='neta_create_page'>
-                 <a href='/netas/ncreate' class="text-blue-400 hover:text-blue-800 transition-all duration-300">新規ネタ登録ページ</a>
-                 
-                  </div>
+                <div class='combination_create_page'>
+                    <a href='/combinations/ccreate' class="text-blue-400 hover:text-blue-800 transition-all duration-300">新規お笑いコンビ(トリオ)登録ページ</a>
+                </div>
+        
+                
                  
                  <!--<div class="office_create_page">-->
                  <!--       <a href="/offices/ocreate" class="text-blue-400 hover:text-blue-800 transition-all duration-300">-->
@@ -80,9 +80,7 @@
                  <!--       </a>-->
                  <!--</div>-->
         
-                 <div class='rank'>
-                <a href='/neta/rank' class="text-blue-400 hover:text-blue-800 transition-all duration-300">ネタランキング</a>
-                 </div>
+                
             </div>
                 {{ $slot }}
             </main>
