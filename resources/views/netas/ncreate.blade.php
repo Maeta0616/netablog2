6d1c23fa46760fdeeb751852a4a3a1aad56094b7
Maeta0616 <maeryo616@gmail.com>
2022-09-29 12:51:03 +0900

    ネタ登録&編集ページのtailwind編集

--- resources/views/netas/ncreate.blade.php
+++ resources/views/netas/ncreate.blade.php
@@ -3,7 +3,7 @@
         　<h1>Neta Regist Page</h1> 
     </x-slot>
         
-        <h1 class="text-2xl">Neta Regist</h1>
+        <h1 class="text-3xl">ネタ登録ページ</h1>
         <form action="/netas" method="POST">
             @csrf
             <!----認めていないサイトからのアクセスを認めない-->
@@ -11,41 +11,54 @@
                 <p class="text-red-500">目的のお笑いコンビ(トリオ)が見つからない場合、「新規お笑いコンビ(トリオ)登録ページ」から登録してください</p>
             </div>
             
-            <div class='name'>
-                <h1 class="text-2xl">ネタ名</h1>
-                <input type='text' name='netas[name]' placeholder='ネタ名' value='{{old('netas.name')}}'/>
-              <p class="title__error" style="color:red">{{ $errors->first('netas.name') }}</p>
+            <div class="mb-6">
+                <label for="default-input" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">ネタ名</label>
+                <input type="text" name='netas[name]'  placeholder='ネタ名' value='{{old('netas.name')}}'
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.name') }}</p>
             </div>
             
             <div class='combination'>
-                <h1 class="text-2xl">コンビ名</h1>
-                <select name='netas[combination_id]'>
+                <label for="combination_select" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">芸名</label>
+                <select name='netas[combination_id]' 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
+                w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose a Comedian</option>
                     @foreach($combinations as $combination)
                     <option value='{{$combination->id}}'>{{$combination->name}}</option>
                     @endforeach
                 </select>
-            <p class="combination__error" style="color:red">{{ $errors->first('netas.combination_id')}}</p>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.combination_id') }}</p>
             </div>
             
-            <div class'genre'>
-                <h1 class="text-2xl">ジャンル</h1>
-                <select name='netas[genre_id]'>
+            <div class='genre'>
+                <label for="combination_select" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">ジャンル</label>
+                <select name='netas[genre_id]' 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
+                w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose a genre</option>
                     @foreach($genres as $genre)
                     <option value='{{$genre->id}}'>{{$genre->name}}</option>
                     @endforeach
                 </select>
-            <p class='genre_error' style='coler:red'>{{$errors->first('netas.genre_id')}}</p>    
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.genre_id') }}</p>
             </div>
             
             <div class='overview'>
-                <h1 class="text-2xl">ネタ内容</h2>
-                <textarea name='netas[overview]' placeholder='ネタ概要'>{{old('netas[overview]')}}</textarea>
-                <p class='text-red-500' style='coler:red'>{{$errors->first('netas.overview')}}</p>
+                <label for="message" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">ネタ内容</label>
+                <textarea name="netas[overview]" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
+                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='ネタ概要'></textarea>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.overview')}}</p>
             </div>
             
             <div class='anker'>
-                <h1 class="text-2xl">Link(Youtube推奨)</h2>
-                <textarea name='netas[anker]' placeholder='https~'></textarea>
+                <label for="message" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Link(Youtube推奨)</label>
+                <textarea name="netas[anker]" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
+                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='ネタ動画のURIを貼り付けてみよう！'></textarea>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.anke')}}</p>
             </div>
             
             <input type='submit' value='登録' class="px-4 py-1 rounded-md bg-blue-600 text-white cursor-pointer hover:bg-blue-800 transition-all duration-300"/>
--- resources/views/netas/nedit.blade.php
+++ resources/views/netas/nedit.blade.php
@@ -13,41 +13,54 @@
                 <p class="text-red-500">目的のお笑いコンビ(トリオ)が見つからない場合、「新規お笑いコンビ(トリオ)登録」から登録してください</p>
             </div>
             
-            <div class='name'>
-                <h1 class="text-2xl">ネタ名</h1>
-                <input type='text' name='netas[name]' placeholder='ネタ名' value='{{$neta->name}}'/>
-              <p class="text-red-500" style="color:red">{{ $errors->first('netas.name') }}</p>
+            <div class="mb-6">
+                <label for="default-input" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">ネタ名</label>
+                <input type="text" name='netas[name]'  placeholder='ネタ名' value='{{$neta->name}}'
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.name') }}</p>
             </div>
             
             <div class='combination'>
-                <h1 class="text-2xl">コンビ名</h1>
-                <select name='netas[combination_id]'>
+                <label for="combination_select" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">芸名</label>
+                <select name='netas[combination_id]' 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
+                w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose a Comedian</option>
                     @foreach($combinations as $combination)
                     <option value='{{$combination->id}}'>{{$combination->name}}</option>
                     @endforeach
                 </select>
-            <p class="tect-red-500" style="color:red">{{ $errors->first('netas.combination_id')}}</p>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.combination_id') }}</p>
             </div>
             
             <div class='genre'>
-                <h1 class="text-2xl">ジャンル</h1>
-                <select name='netas[genre_id]'>
+                <label for="combination_select" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">ジャンル</label>
+                <select name='netas[genre_id]' 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
+                w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choose a genre</option>
                     @foreach($genres as $genre)
                     <option value='{{$genre->id}}'>{{$genre->name}}</option>
                     @endforeach
                 </select>
-            <p class='text-red-500' style='coler:red'>{{$errors->first('netas.genre_id')}}</p>    
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.genre_id') }}</p>
             </div>
             
             <div class='overview'>
-                <h1 class="text-2xl">ネタ内容</h1>
-                <textarea name='netas[overview]' placeholder='ネタ内容'>{{$neta->overview}}</textarea>
-                <p class='text-red-500'style='coler:red'>{{$errors->first('netas.overview')}}</p>
+                <label for="message" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">ネタ内容</label>
+                <textarea name="netas[overview]" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
+                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='ネタ概要'>{{$neta->overview}}</textarea>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.overview')}}</p>
             </div>
             
             <div class='anker'>
-                <h1 class="text-2xl">Link(Youtube推奨)</h2>
-                <textarea name='netas[anker]' placeholder='https~'>{{$neta->anker}}</textarea>
+                <label for="message" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Link(Youtube推奨)</label>
+                <textarea name="netas[anker]" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
+                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='ネタ動画のURIを貼り付けてみよう！'></textarea>
+                <p class="text-red-500" style="color:red">{{ $errors->first('netas.anke')}}</p>
             </div>
             
             <input type='submit' value='再登録' class="px-4 py-1 rounded-md bg-blue-600 text-white hover:bg-blue-800 transition-all duration-300"/>
