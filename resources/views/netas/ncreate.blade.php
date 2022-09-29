bde1769c0e54d34de63ce40fffc87f1b2181f05f
Maeta0616 <maeryo616@gmail.com>
2022-09-29 14:39:05 +0900

    テキストフォント修正

--- resources/views/netas/ncreate.blade.php
+++ resources/views/netas/ncreate.blade.php
@@ -54,7 +54,7 @@
             </div>
             
             <div class='anker'>
-                <label for="message" class="text-2xl block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Link(Youtube推奨)</label>
+                <label for="message" class="text-2xl block mb-2 text-sm font-bold text-gray-900 dark:text-gray-400">Link(Youtube推奨)</label>
                 <textarea name="netas[anker]" rows="2" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 
                 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
                 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='ネタ動画のURIを貼り付けてみよう！'></textarea>
