2da5e53410604d3dd471f049c09ba8d866d514b3
Maeta0616 <maeryo616@gmail.com>
2022-10-01 20:01:02 +0900

    Reviewの編集権限をそのUserだけに与えるようにした

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Review;
+use App\Policies\PostPolicy;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Post' => 'App\Policies\PostPolicy',
+        //↓このコードが重要らしい//
+        Review::class=>PostPolicy::class,
     ];
 
     /**
